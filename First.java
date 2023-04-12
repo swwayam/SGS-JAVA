9b64e75badf8896a5a23d6add15ac4ad197c1ebe
swwayam <codingswayam24@gmail.com>
2023-04-12 19:21:48 +0530

    SGS-JAVA

---
+++ First.java
@@ -0,0 +1,38 @@
+class Calculator{
+    int addition(int num1,int num2){
+        return num1 + num2;
+    }
+
+    int subtraction(int num1 ,int num2){
+        return num1 - num2;
+    }
+
+    multiplication(){
+
+    }
+
+    division(){
+
+    }
+
+    modulus(){
+
+    }
+}
+
+
+// Filename should be same as the class name
+
+class First{
+
+
+
+    // there should be a main method present 
+    public static void main(String[] args) {
+        System.out.println("First Java Class");
+        System.out.println("Hello World");
+
+    }
+    
+}
+
